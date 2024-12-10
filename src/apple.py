6c6a26ef4c08c42eb6fc84c529d6c12a8fecdd2a
volkan <zerovolkan@proton.me>
2024-12-10 20:07:23 +0400

    generate!

--- src/apple.py
+++ src/apple.py
@@ -1,18 +1,43 @@
+import pyglet as pg
+import random as rm
+
 class Apples:
-    def __init__(self, xlen, ylen, side):
-        self.apples: list[tuple[int, int]]
+    def __init__(self, xlen, ylen, side, batch):
+        self.apples: list[tuple[int, int]] = []
 
         self.xlen = xlen
         self.ylen = ylen
         self.side = side
 
-    def collision(self, x, y):
-        for apple in self.apples:
-            if apple == (x, y):
-                pass
+        self.batch = batch
 
-    def remove(self, x, y):
+    def collision(self, x, y) -> bool:
+        for apple in self.apples:
+            if apple[0] * self.side == x and apple[1] * self.side == y:
+                return True
+        return False
+
+    def remove(self, x, y) -> None:
         self.apples.remove((x, y))
 
-    def generate(self):
-        ...
+    def add(self, x, y) -> None:
+        self.apples.append((x, y))
+
+    def generate(self) -> None:
+        while True:
+            x, y = rm.randint(0, self.xlen), rm.randint(0, self.ylen)
+            if self.collision(x, y):
+                continue
+            self.add(rm.randint(0, self.xlen), rm.randint(0, self.ylen))
+
+            break
+
+    def draw(self):
+        for apple in self.apples:
+            pg.shapes.Rectangle(
+                apple[0] * self.side, apple[1] * self.side,
+                self.side,
+                self.side,
+                batch=self.batch,
+                color=(255, 0, 0)
+            ).draw()
--- src/main.py
+++ src/main.py
@@ -9,18 +9,25 @@ side = 40
 window = pg.window.Window(x * side, y * side)
 batch = pg.graphics.Batch()
 snake = Snake(x // 2, y // 2, side, batch)
-apples = Apples(x, y, side)
-
+apples = Apples(x, y, side, batch)
+[apples.generate() for i in range(4)]
 
 @window.event
 def on_key_press(symbol, modifiers):
     snake.direction = direction if (direction := Direction.from_key(symbol)) != Direction.none else snake.direction
 
-
+# GAME LOOP
 @window.event
 def on_draw():
-    snake.move()
+    if apples.collision(*snake.position()):
+        apples.remove(*map(lambda i: i // 40, snake.position()))
+        snake.move(True)
+        apples.generate()
+    else:
+        snake.move()
+
     window.clear()
+    apples.draw()
     snake.draw()
 
 
